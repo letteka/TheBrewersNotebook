5b243727b1fcc1ed77c787e09e088902083d3248
Andrew Kettel <akettel@parthenonsoftware.com>
2014-05-20 12:07:54 -0700

    Fixing the testing build.

---
+++ TheBrewersNotebookTests/TheBrewersNotebookTests.m
@@ -0,0 +1,35 @@
+//
+//  BrewdayHelperTests.m
+//  BrewdayHelperTests
+//
+//  Created by Andrew on 2/20/14.
+//  Copyright (c) 2014 Letteka Brewing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TheBrewersNotebookTests : XCTestCase
+
+@end
+
+@implementation TheBrewersNotebookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
