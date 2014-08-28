fd06649046aba04a8bf424774ec70ee4988d2be1
Andrew Kettel <akettel@parthenonsoftware.com>
2014-08-27 17:40:55 -0700

    Adding in a temp timer.

--- TheBrewersNotebook/Controller/TBNRecipeTimerViewController.h
+++ TheBrewersNotebook/Controller/TBNRecipeTimerViewController.h
@@ -13,9 +13,14 @@
 @interface TBNRecipeTimerViewController : UIViewController
 
 @property (nonatomic, strong) IBOutlet UIPageControl* pageControl;
+@property (nonatomic, strong) IBOutlet UIButton* startButton;
+@property (nonatomic, strong) IBOutlet UIButton* stopButton;
+@property (nonatomic, strong) IBOutlet UILabel* timerLabel;
 
 @property (nonatomic, strong) TBNRecipe* recipe;
 
 - (IBAction)nextButtonPressed:(id)sender;
+- (IBAction)startTimerPressed:(id)sender;
+- (IBAction)stopTimerPressed:(id)sender;
 
 @end
--- TheBrewersNotebook/Controller/TBNRecipeTimerViewController.m
+++ TheBrewersNotebook/Controller/TBNRecipeTimerViewController.m
@@ -12,12 +12,20 @@
 
 @end
 
-@implementation TBNRecipeTimerViewController
+@implementation TBNRecipeTimerViewController {
+    NSTimer *_timer;
+    NSInteger _minutes;
+    NSInteger _seconds;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.navigationItem setTitle:self.recipe.name];
+    
+    // temp data
+    _minutes = 2;
+    [self setTimerText];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,11 +33,58 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)timerFires{
+    if (_minutes > 0 || _seconds > 0)
+    {
+        if (_seconds > 0) {
+            _seconds--;
+        }
+        else if (_seconds == 0) {
+            _seconds = 59;
+            _minutes--;
+        }
+        [self setTimerText];
+    }
+    else
+    {
+        self.timerLabel.text = @"00:00";
+        [_timer invalidate];
+        _timer = nil;
+    }
+}
+
+- (void)setTimerText
+{
+    self.timerLabel.text = [NSString stringWithFormat:@"%02d:%02d", _minutes, _seconds];
+}
+
 #pragma mark - IBActions
 - (IBAction)nextButtonPressed:(id)sender {
     [UIView animateWithDuration:0.5 animations:^(void) {
         [self.pageControl setCurrentPage:[self.pageControl currentPage] + 1];
     }];
+}
+
+- (IBAction)startTimerPressed:(id)sender
+{
+    [self.stopButton setUserInteractionEnabled:YES];
+    [self.startButton setUserInteractionEnabled:NO];
+    
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1
+                                             target:self
+                                           selector:@selector(timerFires)
+                                           userInfo:nil
+                                            repeats:YES];
+    [_timer fire];
+}
+
+- (IBAction)stopTimerPressed:(id)sender
+{
+    [self.startButton setUserInteractionEnabled:YES];
+    [self.stopButton setUserInteractionEnabled:NO];
+    
+    [_timer invalidate];
+    _timer = nil;
 }
 
 /*
